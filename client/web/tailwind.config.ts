2e42fa5b73a154cb51a7bad8d6dda39ef113fa75
ishaan <ishaankhurana123@gmail.com>
2023-08-29 21:01:24 +0530

    feat: saving some change

--- client/web/tailwind.config.ts
+++ client/web/tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        primary: "#33313B",
+        primary: "#201f25",
         "custom-blue": "#4592AF",
       },
       fontFamily: {
