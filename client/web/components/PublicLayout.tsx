ba8735867bb1e5fd0120500d7b4b810afca3c20d
ik04 <ishaankhurana123@gmail.com>
2023-09-20 08:26:20 +0530

    feat: make role enum, setup migration, create login and register functions

--- client/web/components/PublicLayout.tsx
+++ client/web/components/PublicLayout.tsx
@@ -18,16 +18,16 @@ const PublicLayout: React.FC<LayoutProps> = ({ children }) => {
       <div className="flex">
         <div className="flex text-white h-[91vh] w-[400px] border-2 border-x-[#275464] border-t-0 border-b-[#275464]">
           <div className="h-full flex items-center justify-center space-y-[14rem] w-full flex-col">
-            <div className="flex items-center gap-5 hover:-translate-y-2 duration-300 ease-in-out transition">
+            <div className="flex items-center gap-5 hover:-translate-y-2 duration-300 ease-in-out transition cursor-pointer">
               <Image src={Book} alt="book" width={45} height={45} />
               <p className="text-2xl font-base">Assignment</p>
             </div>
-            <div className="flex items-center gap-5 hover:-translate-y-2 duration-300 ease-in-out transition">
+            <div className="flex items-center gap-5 hover:-translate-y-2 duration-300 ease-in-out transition cursor-pointer">
               <Image src={Test} alt="paper" />
 
               <p className="text-2xl font-base">Test Papers</p>
             </div>
-            <div className="flex items-center gap-5 hover:-translate-y-2 duration-300 ease-in-out transition">
+            <div className="flex items-center gap-5 hover:-translate-y-2 duration-300 ease-in-out transition cursor-pointer">
               <Image src={Past} alt="book" />
               <p className="text-2xl font-base">Past Docs</p>
             </div>
